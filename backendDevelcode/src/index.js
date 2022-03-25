34ac8eb90342c84d5fe43648369818d0ba1288dd
Giliardi <giliardi.oliveira@itajai.sc.gov.br>
2022-03-25 18:35:56 -0300

    [chore] Adicionado o acesso ao banco de dados

--- backendDevelcode/src/index.js
+++ backendDevelcode/src/index.js
@@ -3,6 +3,16 @@ const bodyParser = require('body-parser');
 const app = express()
 const port = 3000
 
+const Pool = require('pg').Pool
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'users',
+    password: '',
+    port: 5432,
+})
+
+
 app.use(bodyParser.json())
 app.use(
     bodyParser.urlencoded({
